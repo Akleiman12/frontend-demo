abc4c7a3c509d489318872011affd9787d34c5ff
akleiman12 <asherkleiman@gmail.com>
2018-09-20 19:30:51 -0400

    delete component completed

--- src/app/delete/delete.component.ts
+++ src/app/delete/delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteComponent implements OnInit {
 
-  constructor() { }
+  public product: Product
+  public productId: string
 
-  ngOnInit() {
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private router: Router) { }
+
+  async ngOnInit() {
+    this.activatedRoute.params.subscribe(res => {
+      this.productId = res.id
+    })
+    this.product = await this.productService.getDetailed(this.productId)
+    if(!this.product){
+      this.router.navigate(['404'])
+    }
+  }
+
+  public async onDelete(){
+    this.productService.delete(this.productId).then(res => {
+      alert("Product deleted successfully!")
+      this.router.navigate([''])
+    }).catch(err => {
+      console.log(err)
+      alert("Error while deleting product.")
+    })
+  }
+
+  public onCancel(){
+    this.router.navigate([''])
   }
 
 }
